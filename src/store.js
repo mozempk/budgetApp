341b09fa2baa65de47a62940e5650379457f3bdb
Laura <laura@imac-de-laura.home>
2019-09-22 17:54:37 +0200

    Added edit expense and income + added currency to store

--- src/store.js
+++ src/store.js
@@ -4,11 +4,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
   state: {
     incomes: [],
     expenses: [],
-    balance: 0
+    balance: 0,
+    selectedCurrency: 'CHF',
+    currencies: ['CHF', 'EUR', 'USD'],
   },
+
   mutations: {
     COMPUTE_BALANCE: state => state.balance = state.incomes.reduce((acc,curr) => {
       return acc+curr.amount
@@ -16,8 +20,24 @@ export default new Vuex.Store({
       return acc+curr.amount
     },0),
     ADD_INCOME: (state, payload) => state.incomes.push(payload),
-    ADD_EXPENSE: (state, payload) => state.expenses.push(payload)
+    ADD_EXPENSE: (state, payload) => state.expenses.push(payload),
+    EDIT_INCOME: (state, payload) => {
+      to_edit = state.incomes.find((element, payload) => {
+        return element.id === payload.id
+      }); // Retrieve previous element
+      to_edit.title = payload.title; // Modify its title
+      to_edit.amount = payload.amount; // Modify its amount
+    },
+    EDIT_EXPENSE: (state, payload) => {
+      to_edit = state.expenses.find((element, payload) => {
+        return element.id === payload.id
+      }); // Retrieve previous element
+      to_edit.title = payload.title;
+      to_edit.amount = payload.amount;
+    },
+    CHANGE_CURRENCY: (state, payload) => state.selectedCurrency = payload,
   },
+
   actions: {
     /*updateBalance: ({commit}) => {
       commit('COMPUTE_BALANCE')
@@ -27,14 +47,27 @@ export default new Vuex.Store({
       commit('COMPUTE_BALANCE');
     },
     addExpense({commit},payload){
-      commit('ADD_EXPENSE',payload)
+      commit('ADD_EXPENSE',payload);
       commit('COMPUTE_BALANCE');
-    }
+    },
+    editIncome({commit},payload){
+      commit('EDIT_INCOME',payload);
+      commit('COMPUTE_BALANCE');
+    },
+    editExpense({commit},payload){
+      commit('EDIT_EXPENSE',payload);
+      commit('COMPUTE_BALANCE');
+    },
+    changeCurrency({commit}, payload){
+      commit('CHANGE_CURRENCY',payload);
+    },
 
   },
+
   getters: {
     incomes: state => state.incomes,
     expenses: state => state.expenses,
     balance: state => state.balance.toString(),
   }
+
 })
